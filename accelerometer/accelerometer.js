5e7ba75fbd0c6375b0bcd043ee3b7aa454587b26
Cristina <cristina.colon+git@gmail.com>
2015-08-14 15:33:58 -0400

    accelerometer responds to a push

--- accelerometer/accelerometer.js
+++ accelerometer/accelerometer.js
@@ -5,17 +5,23 @@
 This basic accelerometer example logs a stream
 of x, y, and z data from the accelerometer
 *********************************************/
-/*
+
 var tessel = require('tessel');
 var accel = require('accel-mma84').use(tessel.port['C']);
+var pushed = true;
 
 // Initialize the accelerometer.
 accel.on('ready', function () {
     // Stream accelerometer data
   accel.on('data', function loop (xyz) {
-    console.log('x:', xyz[0].toFixed(2),
+    if (pushed && Math.abs(xyz[2].toFixed(2))>0.1) {
+      console.log('I have been pushed.');
+      pushed = false;
+      setTimeout(function(){pushed=true;},2000);
+    }
+    /*console.log('x:', xyz[0].toFixed(2),
       'y:', xyz[1].toFixed(2),
-      'z:', xyz[2].toFixed(2));
+      'z:', xyz[2].toFixed(2));*/
   });
 
 });
@@ -23,7 +29,7 @@ accel.on('ready', function () {
 accel.on('error', function(err){
   console.log('Error:', err);
 });
-*/
+
 // Any copyright is dedicated to the Public Domain.
 // http://creativecommons.org/publicdomain/zero/1.0/
 
@@ -33,11 +39,9 @@ a stream of x, y, and z data, then stops the
 stream, changes the polling rate, and resumes
 streaming from the accelerometer
 *********************************************/
-
+/*
 var tessel = require('tessel');
 var accel = require('accel-mma84').use(tessel.port['C']);
-
-//var accel = require('../').use(tessel.port['A']); // Replace '../' with 'accel-mma84' in your own code
 
 // Initialize the accelerometer.
 accel.on('ready', function () {
@@ -64,4 +68,4 @@ accel.on('ready', function () {
 
 accel.on('error', function(err){
   console.log('Error:', err);
-});
+});*/
