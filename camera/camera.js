6f8308d0a066100aabb3181c4b5665c0996dfff4
Cristina <cristina.colon+git@gmail.com>
2015-08-14 15:23:25 -0400

    camera takes picture and climate measurements

--- camera/camera.js
+++ camera/camera.js
@@ -9,12 +9,15 @@ flag, the picture is saved to that directory.
 
 var tessel = require('tessel');
 var camera = require('camera-vc0706').use(tessel.port['A']);
+var climatelib = require('climate-si7020');
+var climate = climatelib.use(tessel.port['B']);
 
 var notificationLED = tessel.led[3]; // Set up an LED to notify when we're taking a picture
 
 // Wait for the camera module to say it's ready
 camera.on('ready', function() {
-  notificationLED.high();
+  climate.on('ready',function(){
+    notificationLED.high();
   // Take the picture
   camera.takePicture(function(err, image) {
     if (err) {
@@ -30,9 +33,27 @@ camera.on('ready', function() {
       // Turn the camera off to end the script
       camera.disable();
     }
-  });
-});
+    console.log('Connected to si7020');
+    climate.readTemperature('f', function (err, temp) {
+      climate.readHumidity(function (err, humid) {
+        console.log('Degrees:', temp.toFixed(4) + 'F', 'Humidity:', humid.toFixed(4) + '%RH');
+        setTimeout(loop, 300);
+      });
+    });
+  });// camera take picture
+  })
+}); //camera on ready
 
 camera.on('error', function(err) {
   console.error(err);
+});
+
+
+
+climate.on('ready', function () {
+  
+});
+
+climate.on('error', function(err) {
+  console.log('error connecting module', err);
 });
